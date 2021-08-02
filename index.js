3599b9503c0bddf281c4f38094b68a2673edf545
jousethen <jthen93@gmail.com>
2021-08-02 16:33:38 -0400

    commit tree 80146ef968000b7f8d2b7fdb8680e6d2911d998d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let arr = [...drivers, name];
+  return arr;
+}
+
+function prependDriver(name) {
+  let arr = [name, ...drivers];
+  return arr;
+}
+
+function removeLastDriver() {
+
+}
